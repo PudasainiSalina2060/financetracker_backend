5dfe5fc7659ba65cd96306787548a08aac5d4d03
Salina2060 <pudasainisalina22@gmail.com>
2025-11-26 17:09:32 +0545

    chore: initial backend setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Finance Tracker Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
