88b2a28f3cbd90343eaa77875a7a61bea19e56ff
PudasainiSalina2060 <pudasainisalina22@gmail.com>
2026-01-09 23:56:18 +0545

    add password reset token model to Prisma schema

---
+++ prisma/migrations/20260109170018_add_password_reset_token/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "reset_id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "token_hash" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "used" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("reset_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PasswordResetToken" ADD CONSTRAINT "PasswordResetToken_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
